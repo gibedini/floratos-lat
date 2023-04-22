08422ffbd0ebd5631952719debd6fe936d34a3e9
gibedini <gianni.bedini@unipi.it>
2023-04-22 23:03:39 +0200

    changed ns_florichness to reflect new floratos_south column set

--- R/ns_florichness.R
+++ R/ns_florichness.R
@@ -1,29 +1,25 @@
-#' Floristic richness
+#' Calculate floristi richness from occurrence records
 #'
-#' @param ns_df a dataframe with at least two columns, viz. combination and esotic
+#' @param ns_sf a dataframe with at least two columns, viz. closest_match and a_status.
+#' Column \code{closest_match} contains scientific names standarised according the
+#' Checklist of the Italian flora. Column \code{a_status} contain the alien status
+#' according to the same Checklist.
+#' @param ns_label a character string to serve as label for the data in the second column
+#' of the output dataframe.
 #'
-#' @return a dataframe with one row and five columns containing number of records,
+#' @return a dataframe with six rows and two columns containing number of records,
 #' number of species, number of native, and number of casual, naturalised, and invasive aliens.
 #' @export ns_florichness
 #'
-#' @examples ns_florichness(floratos_south)
-ns_florichness <- function(ns_df) {
-  exot_df <- ns_exotic()
-  nrecs <- nrow(ns_df)
-  spec_df <- data.frame(combination = unique(ns_df$combination))
-  spec_ckl <- dplyr::inner_join(spec_df,floratos_ckl, by = c("combination" = "entita"))
-  spec_anti <- dplyr::anti_join(spec_df,floratos_ckl, by = c("combination" = "entita"))
-  nspec <- length(unique(ns_df$combination))
-  nnative <- unicomb(ns_df,exot_df$defs[1])
-  ncas <-  unicomb(ns_df, exot_df$defs[2])
-  nnatur <-  unicomb(ns_df, exot_df$defs[3])
-  ninv <-  unicomb(ns_df, exot_df$defs[4])
-  return(data.frame(n_of_recs = nrecs,n_of_species = nspec,
-                    native = nnative,casual_alien = ncas,
-                    naturalized = nnatur, invasive_alien = ninv))
+#' @examples ns_florichness(floratos_south,"south")
+ns_florichness <- function(ns_sf,ns_label) {
+  ns_df <- sf::st_drop_geometry(ns_sf)
+  nsf_a <- tibble::tibble(param = c("nrec","nspe"),
+                        !!(ns_label) := c(nrow(ns_df),length(unique(ns_df$closest_match))))
+  nsf_b <- dplyr::distinct(ns_df,closest_match,a_status) %>%
+    dplyr::group_by(a_status) %>%
+    dplyr::summarise(n=dplyr::n()) %>%
+    dplyr::rename(param = a_status,!!(ns_label) := n)
+  return(dplyr::union(nsf_a,nsf_b))
 }
 
-unicomb <- function(d,e) {
-  dx <- d[d$esotic == e,]
-  return(length(unique(dx$combination)))
-}
--- tests/testthat/test-ns_florichness.R
+++ tests/testthat/test-ns_florichness.R
@@ -1,3 +1,3 @@
 test_that("return dataframe has six columns", {
-  expect_equal(ncol(ns_florichness(floratos_south)), 6)
+  expect_equal(ncol(ns_florichness(floratos_south,"south")), 2)
 })
